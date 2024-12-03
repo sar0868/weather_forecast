c17ed9bd6e1cbd686ab59adaef5d3466e48ce52a
sar0868 <safarov-ar@yandex.ru>
2024-12-03 23:36:56 +0300

    add eslint html and css

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import jest from "eslint-plugin-jest";
 import eslintRecommended from "eslint-plugin-prettier/recommended";
+import html from "eslint-plugin-html";
+import * as cssPlugin from "eslint-plugin-css";
 
 export default [
   eslintRecommended,
@@ -18,4 +20,9 @@ export default [
     files: ["src/**/*.test.js"],
     ...jest.configs["flat/recommended"],
   },
+  {
+    files: ["src/**/*.html"],
+    plugins: { html },
+  },
+  cssPlugin.configs["flat/recommended"],
 ];
