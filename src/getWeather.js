ee2a7f89a55fd3533ee844afd3d65116277cd788
sar0868 <safarov-ar@yandex.ru>
2024-12-18 00:10:47 +0300

    edit getWeather.js

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,7 +1,7 @@
 const API_KEY = "6cb973b96821fbec8a012c668b8f4700";
 
 export async function getWeather(city) {
-  const path = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`;
+  const path = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`;
 
   const response = await fetch(path);
 
