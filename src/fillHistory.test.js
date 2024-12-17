78de4acd0f69d3bdd3b070d56465c8f5d12bc6dd
sar0868 <safarov-ar@yandex.ru>
2024-12-17 12:58:51 +0300

    fillHistory test done

--- src/fillHistory.test.js
+++ src/fillHistory.test.js
@@ -1,37 +1,10 @@
 import { fillHistory } from "./fillHistory";
 
-let mockStorage = [{}];
-
-/* global global */
 describe("test fillHistory", () => {
-  beforeAll(() => {
-    global.Storage.prototype.setItem = jest.fn((key, value) => {
-      mockStorage.push({ key: value });
-    });
-    global.Storage.prototype.getItem = jest.fn((key) => {
-      for (const item of mockStorage) {
-        if (item.k === key) {
-          return item.k;
-        }
-      }
-    });
-    global.Storage.prototype.key = jest.fn((ind) => {
-      mockStorage[ind];
-    });
-  });
-
-  beforeEach(() => {
-    mockStorage = [{}];
-  });
-  afterAll(() => {
-    global.Storage.prototype.setItem.mockReset();
-    global.Storage.prototype.getItem.mockReset();
-    global.Storage.prototype.key.mockReset();
-  });
   it("test create list history", () => {
     const el = document.createElement("div");
     localStorage.setItem("London", "hello");
-    const expected = `<p class='key'>London</p>`;
+    const expected = `<p class="key">London</p>`;
 
     fillHistory(el);
     expect(el.innerHTML).toEqual(expected);
--- src/weather.test.js
+++ src/weather.test.js
@@ -1,43 +1,5 @@
 import { weather } from "./weather";
 
-import { mockWeather } from "./mock.weather";
-
-/* global global */
-global.fetch = jest.fn(() => {
-  return Promise.resolve({
-    ok: true,
-    json: () => Promise.resolve(mockWeather),
-  });
-});
-
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = value;
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
-
-global.localStorage = jest.fn(() => {
-  return new LocalStorageMock();
-});
-
-global.localStorage = new LocalStorageMock();
-
 describe("test get weather", () => {
   const el = document.createElement("main");
   beforeAll(() => {
@@ -47,52 +9,10 @@ describe("test get weather", () => {
   describe("Create markup", () => {
     it("test create markup", () => {
       expect(el.querySelector("input")).toBeTruthy();
-      expect(el.querySelector("button")).toBeTruthy();
-      expect(el.querySelector("button")).toBeTruthy();
-      expect(el.querySelector("button")).toBeTruthy();
+      expect(el.querySelector("#button")).toBeTruthy();
+      expect(el.querySelector("#info")).toBeTruthy();
+      expect(el.querySelector("#map")).toBeTruthy();
+      expect(el.querySelector("#list_history")).toBeTruthy();
     });
-  });
-
-  describe("Test get info from localStorage after click", () => {
-    afterEach(() => {
-      localStorage.clear();
-      jest.clearAllMocks();
-    });
-
-    it("should data is added into local storage", () => {
-      const city = "London";
-      el.querySelector("input").innerHTML = city;
-      el.querySelector("#button").click();
-      console.log(localStorage.length);
-      // const result = localStorage.getItem(city);
-      // expect(localStorage.getItem(city)).toEqual(JSON.stringify(mockWeather));
-    });
-  });
-
-  describe("Test get map from localStorage", () => {
-    it.todo("test get map");
-  });
-
-  describe("Test get info from localStorage", () => {
-    beforeEach(() => {
-      localStorage.clear();
-    });
-
-    it("should data is added into local storage", () => {
-      const mockId = "1";
-      const mockJson = { data: "json data" };
-
-      localStorage.setItem(mockId, JSON.stringify(mockJson));
-
-      const result = localStorage.key(0);
-      expect(result).toBe(mockId);
-      expect(localStorage.getItem(mockId)).toEqual(JSON.stringify(mockJson));
-    });
-  });
-
-  describe("localStorage", () => {
-    it.todo("test set in localStorage");
-
-    it.todo("test get in localStorage");
   });
 });
