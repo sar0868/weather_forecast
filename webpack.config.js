dec67c46fdf3d788e14c781497a84a14070547b1
sar0868 <safarov-ar@yandex.ru>
2024-12-17 20:14:36 +0300

    css in progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 // const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const path = require("path");
@@ -7,27 +8,20 @@ module.exports = {
   entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js",
+    filename: "app.js",
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
-  //   module: {
-  //     module: {
-  //       rules: [
-  //         {
-  //           test: /.s?css$/,
-  //           use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-  //         },
-  //       ],
-  //     },
-  //     optimization: {
-  //       minimizer: [
-  //         // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
-  //         // `...`,
-  //         new CssMinimizerPlugin(),
-  //       ],
-  //     },
-  //     plugins: [new MiniCssExtractPlugin()],
-  //   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   devServer: {
     static: {
       directory: path.join(__dirname, "dist"),
