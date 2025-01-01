6e3e421aa01f686363ca09c407469931297f9b45
sar0868 <safarov-ar@yandex.ru>
2025-01-01 19:25:46 +0300

    resolved comment pre-huck for fetch

--- src/getWeather.test.js
+++ src/getWeather.test.js
@@ -2,14 +2,15 @@ import { getWeather } from "./getWeather";
 import { mockWeather } from "./mock.weather";
 
 /* global global */
-global.fetch = jest.fn(() => {
-  return Promise.resolve({
-    ok: true,
-    json: () => Promise.resolve(mockWeather),
-  });
-});
-
 describe("test get weather", () => {
+  beforeAll(() => {
+    global.fetch = jest.fn(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => Promise.resolve(mockWeather),
+      });
+    });
+  });
   afterEach(() => {
     jest.clearAllMocks();
   });
